f728a9cfbf1216d08fb041d6ed09b7ada49b957e
안영진 <158057603+anyoungjin20040106@users.noreply.github.com>
2025-06-07 20:45:56 +0900

    update FeatureData

--- FeatureData.R
+++ FeatureData.R
@@ -1,13 +1,11 @@
 # 데이터 불러오기
 df <- read.csv("NintendoGames.csv", stringsAsFactors = FALSE)
 tf <- read.csv("data.csv", stringsAsFactors = FALSE)
-
-# 필요한 열만 선택
-df <- df[, c("meta_score", "title", "platform", "user_score")]
-tf <- tf[, c("meta_score", "title", "platform", "user_score")]
-
 # 데이터 결합
 df <- rbind(df, tf)
+write.csv(df, "rbind.csv", row.names = FALSE)
+
+df <- df[, c("meta_score", "platform", "user_score")]
 
 # 'platform'과 'title'에 NA가 있는 행 제거
 df <- df[!is.na(df$platform), ]
